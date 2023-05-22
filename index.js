535862a94d8f2ced3b0a47b664e2db8487173bc3
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-05-22 14:31:25 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+// Test 1
+const returnFirstTwoDrivers = arr => [arr[0], arr[1]];
+
+// Test 2
+const returnLastTwoDrivers = arr => [arr[arr.length-2], arr[arr.length-1]];
+
+// Test 3 - 5
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// Test 6 - 7
+function createFareMultiplier(num) {
+    return function(fare){return fare*num};
+}
+
+// Test 8 - 9
+const fareDoubler = createFareMultiplier(2);
+
+// Test 10 - 11
+const fareTripler = createFareMultiplier(3);
+
+// Test 12 - 13
+const selectDifferentDrivers = (drivers, returnDrivers) => returnDrivers(drivers);
